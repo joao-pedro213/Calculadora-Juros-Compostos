f9614ecd3fa74ad2dace8963994076f6eda2ac22
joao-pedro213 <joao.capobiango213@gmail.com>
2020-08-23 15:44:30 -0300

    Implementando a função para cálculo de juros compostos.

--- src/components/Installments/Installment.js
+++ src/components/Installments/Installment.js
@@ -2,19 +2,41 @@ import React from 'react';
 
 export default function Installment(props) {
   const { reference, initialCapitalValue, compoundInterestValue } = props;
+  // prettier-ignore
+  const calculateCompoundInterest = (initialCapital, installment, compoundInterest) => {
+    let amount = initialCapital * Math.pow(1 + compoundInterest/100, installment);
+    amount = (amount -initialCapital);
+    return amount;
+  };
+
+  const calculatePercentageFrom = (total, value) => {
+    const percentage = (value * 100) / total;
+    return percentage;
+  };
+
+  // prettier-ignore
+  const amount = calculateCompoundInterest(initialCapitalValue, reference, compoundInterestValue);
+  const isNegative = Math.sign(amount) === -1 ? true : false;
+
   return (
     <div style={styles.border}>
       <p>
-        <span>Parcelas: </span>
+        <span>Parcela: </span>
         <strong>{reference}</strong>
       </p>
       <p>
-        <span>Capital inicial: </span>
-        {initialCapitalValue}
+        <span>Total: </span>
+        {(parseInt(initialCapitalValue) + amount).toFixed(2)}
       </p>
       <p>
-        <span>Juros Comspostos: </span>
-        {compoundInterestValue}
+        <span>{!isNegative ? 'Rendimento: ' : 'Depreciação: '}</span>
+
+        {!isNegative
+          ? `R$ ${amount.toFixed(2)}`
+          : `-R$ ${Math.abs(amount).toFixed(2)}`}
+      </p>
+      <p>
+        {`${calculatePercentageFrom(initialCapitalValue, amount).toFixed(2)}%`}
       </p>
     </div>
   );
