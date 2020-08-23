ea0d678f63254865719c4c62df345ff3027de688
joao-pedro213 <joao.capobiango213@gmail.com>
2020-08-23 01:09:48 -0300

    Criando o estado da aplicação.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Header from './components/Header/Header';
 
 export default function App() {
+  const [initialCapital, setInitialCapital] = React.useState(0);
+  const [compountInterest, setCompoundInterest] = React.useState(0);
+  const [tranche, setTranche] = React.useState(0);
+
   return (
     <div className="container center">
       <Header />
