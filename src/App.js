59333dac65e1200744b5fc7341770de0de46f23a
joao-pedro213 <joao.capobiango213@gmail.com>
2020-08-23 02:12:53 -0300

    implementando o componente NumberImput.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import NumberInput from './components/NumberInput/NumberInput';
 
 export default function App() {
   const [initialCapital, setInitialCapital] = React.useState(0);
   const [compountInterest, setCompoundInterest] = React.useState(0);
-  const [tranche, setTranche] = React.useState(0);
+  const [installment, setInstallment] = React.useState(0);
+
+  const handleInitialCapitalInput = (newInitialCapital) => {
+    setInitialCapital(newInitialCapital);
+  };
+  const handleCompundInterestInput = (newCompoundInterest) => {
+    setCompoundInterest(newCompoundInterest);
+  };
+  const handleInstallmentInput = (newInstallment) => {
+    setInstallment(newInstallment);
+  };
 
   return (
     <div className="container center">
       <Header />
+      <div style={styles.inputRow}>
+        <NumberInput
+          idReference="initialCapital"
+          labelName="Capital inicial:"
+          value={initialCapital}
+          onInputChange={handleInitialCapitalInput}
+        />
+        <NumberInput
+          idReference="compoundInterest"
+          labelName="Juros Compostos:"
+          value={compountInterest}
+          onInputChange={handleCompundInterestInput}
+        />
+        <NumberInput
+          idReference="Installment"
+          labelName="Parcelas:"
+          value={installment}
+          onInputChange={handleInstallmentInput}
+        />
+      </div>
     </div>
   );
 }
+
+const styles = {
+  inputRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '30px',
+  },
+};
---
+++ src/components/NumberInput/NumberInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import css from './numberInput.module.css';
+
+export default function NumberInput(props) {
+  const { idReference, labelName, value, onInputChange } = props;
+
+  const handleInputChange = (event) => {
+    onInputChange(event.target.value);
+  };
+
+  const inputClasses = `${css.numberInput} input-field`;
+
+  return (
+    <div className={inputClasses}>
+      <input
+        id={idReference}
+        type="number"
+        value={value}
+        onChange={handleInputChange}
+      />
+      <label className="active" htmlFor={idReference}>
+        {labelName}
+      </label>
+    </div>
+  );
+}
